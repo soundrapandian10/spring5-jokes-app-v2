fe7a606ab6daf623229fc33e3d1267b773e061bd
soundrapandian10 <s.soundrapandian10@gmail.com>
2024-09-14 10:51:04 +0530

    Added Joke Service. Closes #3

---
+++ spring5-jokes-app-v2/src/main/java/com/springframework/spring5_jokes_app_v2/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.springframework.spring5_jokes_app_v2.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
